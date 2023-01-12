62c21b66b1d4fdb8a228590ed63ed089075d1de4
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-12 21:52:36 +0800

    支援hotfix，cog架構完成

--- cogs/general.py
+++ cogs/general.py
@@ -2,26 +2,49 @@ import discord
 from discord import app_commands,Embed
 from discord.ext import commands
 from . import common
+from datetime import datetime, timezone, timedelta
 
 
 
-class BotSystem(commands.Cog):
+class General(commands.Cog):
     def __init__(self, client:commands.Bot):
         self.bot = client
 
 
     @app_commands.command(name = "info", description = "關於Natalie...")
     async def info(self,interaction):
-        await interaction.response.send_message("我還沒想好這裡要寫什麼......")
+        description = """
+        你好!我是Natalie!
+        以下是我擁有的指令:
+        無
+        """
+        await interaction.response.send_message(embed=Embed(title="我是Natalie!",description=description,color=common.bot_color))
 
-    @app_commands.command(name="shutdown", description = "關閉機器人" )
-    async def shutdown(self,interaction):
-        if interaction.user.id == common.bot_owner_id:
-            await interaction.response.send_message(embed=Embed(title="系統操作",description="我要睡著了...",color=common.bot_color))
-            await self.bot.close()
-        else: 
-            await interaction.response.send_message(embed=Embed(title="系統操作",description="權限不足。",color=common.bot_color))
+    @commands.Cog.listener()
+    async def on_voice_state_update(self,member, before, after):
+    #進入語音頻道
+        if after.channel and not before.channel:
+            embed = Embed(title="", description=f"{member.display_name} 進入了 {after.channel.name} 語音頻道", color=common.bot_color)
+            embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+            embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
+            await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
+
+        #離開語音頻道
+        if before.channel and not after.channel:
+            embed = Embed(title="", description=f"{member.display_name} 離開了 {before.channel.name} 語音頻道", color=common.bot_color)
+            embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+            embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
+            await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
+
+        #切換語音頻道
+        if before.channel != after.channel:
+            if before.channel and after.channel:
+                embed = Embed(title="", description=f"{member.display_name} 從 {before.channel.name} 移動到 {after.channel.name} 頻道", color=common.bot_color)
+                embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+                embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
+                await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
+
 
 
 async def setup(client:commands.Bot):
-    await client.add_cog(BotSystem(client))
+    await client.add_cog(General(client))
