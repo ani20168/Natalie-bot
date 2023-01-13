29a3088ad41564e9fac2c56057488513be5d12a8
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-13 18:30:39 +0800

    Update startup.py

--- cogs/startup.py
+++ cogs/startup.py
@@ -2,11 +2,11 @@ import discord
 from discord import app_commands,Embed
 from discord.ext import commands,tasks
 from . import common
-from datetime import datetime, timezone, timedelta
+from datetime import datetime,timezone,timedelta
 import asyncio
 
 
-utc8 = timezone(timedelta(hours=8))
+
 
 class Startup(commands.Cog):
     def __init__(self, client:commands.Bot):
@@ -18,17 +18,19 @@ class Startup(commands.Cog):
         self.clearstardoor.cancel()
         
     #自動鎖定過期的星門
-    @tasks.loop(time=datetime.time(hour=18,tzinfo=utc8))
+    @tasks.loop(hours=1)
     async def clearstardoor(self):
-        list = self.bot.get_channel(1057894690478899330).threads
-        await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description="正在清除過期的星門...",color=common.bot_color))
-        if len(list) != 0:
-            for thread in list:
-                if any( tag.name == "星門" for tag in thread.applied_tags):
-                    await thread.edit(archived=True,locked=True,reason="自動鎖定過期的星門")
-            await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description=f"已鎖定{len(list)}個星門。",color=common.bot_color))
-        else: 
-            await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description="沒有可以鎖定的星門。",color=common.bot_color))
+        nowtime = datetime.now(timezone(timedelta(hours=8)))
+        if nowtime.hour == 18:
+            list = self.bot.get_channel(1057894690478899330).threads
+            await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description="正在清除過期的星門...",color=common.bot_color))
+            if len(list) != 0:
+                for thread in list:
+                    if any( tag.name == "星門" for tag in thread.applied_tags):
+                        await thread.edit(archived=True,locked=True,reason="自動鎖定過期的星門")
+                await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description=f"已鎖定{len(list)}個星門。",color=common.bot_color))
+            else: 
+                await self.bot.get_channel(common.admin_log_channel).send(embed=Embed(title="星門管理員",description="沒有可以鎖定的星門。",color=common.bot_color))
 
     @clearstardoor.before_loop    
     async def before_clearstardoor(self):
