89faa302deaade4abc67222e7d2f3031d35e1b2b
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-13 14:41:08 +0800

    Create startup.py

---
+++ cogs/startup.py
@@ -0,0 +1,27 @@
+import discord
+from discord import app_commands,Embed
+from discord.ext import commands
+from . import common
+from datetime import datetime, timezone, timedelta
+import asyncio
+
+
+
+class Startup(commands.Cog):
+    def __init__(self, client:commands.Bot):
+        self.bot = client
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.get_channel(common.admin_log_channel).send('cog的on_ready測試')
+        await self.testloop()
+
+    async def testloop(self):
+        await self.bot.get_channel(common.admin_log_channel).send('cog的loop測試')
+        await asyncio.sleep(10)
+
+        
+
+async def setup(client:commands.Bot):
+    await client.add_cog(Startup(client))
