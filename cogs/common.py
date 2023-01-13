f049abe528fcd7477de9f7d2e7dbbb346259bdd5
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-13 22:25:50 +0800

    cake id fix

--- cogs/common.py
+++ cogs/common.py
@@ -3,4 +3,4 @@ bot_color = 0x00DFEB                     #embed顏色
 admin_log_channel = 543641756042788864   #admin日誌ID
 mod_log_channel = 1062348474152136714    #管理員日誌ID
 bot_owner_id = 410847926236086272        #我的ID
-cake_emoji_id = 1063433835926274078      #蛋糕ID
+cake_emoji_id = 896670335326371840       #蛋糕ID
