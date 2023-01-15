26d4790145f220736dfb971c39c49da23e25b551
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-15 19:04:52 +0800

    add bot error color

--- cogs/common.py
+++ cogs/common.py
@@ -2,6 +2,7 @@ import json
 
 #這裡放置常用的資料
 bot_color = 0x00DFEB                     #embed顏色
+bot_error_color = 0xFF5151               #出現錯誤訊息的embed顏色
 admin_log_channel = 543641756042788864   #admin日誌ID
 mod_log_channel = 1062348474152136714    #管理員日誌ID
 bot_owner_id = 410847926236086272        #我的ID
