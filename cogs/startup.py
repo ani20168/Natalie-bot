b90b8eadc01bc5d7c8c9d3d8000243918a16cf29
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-01-16 19:15:06 +0800

    Update startup.py

--- cogs/startup.py
+++ cogs/startup.py
@@ -65,10 +65,10 @@ class Startup(commands.Cog):
                 #VIP和MOD獎勵
                 if str(member.id) in data and any(role.id == 419185180134080513 or 605730134531637249 for role in member.roles):
                     data[str(member.id)]["cake"] += 1
+                #直播獎勵
                 if str(member.id) in data and member.activity:
                     if member.activity.type == discord.ActivityType.streaming:
                         data[str(member.id)]["cake"] += 1
-                        await self.bot.get_channel(common.admin_log_channel).send(content="直播中")
 
         common.datawrite(data)
 
