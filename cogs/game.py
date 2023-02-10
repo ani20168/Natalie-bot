0c5cc6dc0572d8b24173172fb8dbfe4bfc98f62a
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-02-10 22:21:52 +0800

    Update game.py

--- cogs/game.py
+++ cogs/game.py
@@ -380,7 +380,7 @@ class BlackJack(commands.Cog):
         self.deck = [{"2": 2}, {"3": 3}, {"4": 4}, {"5": 5}, {"6": 6}, {"7": 7}, {"8": 8}, {"9": 9}, {"10": 10}, {"J": 10}, {"Q": 10}, {"K": 10}, {"A": 11}] * 4
 
     #加牌
-    def deal_card(self,deck, recipient):
+    def deal_card(self,interaction,deck, recipient):
         card = deck.pop()
         recipient.append(card)
 
