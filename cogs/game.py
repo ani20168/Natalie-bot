108436c4cb12ea33184ea0cc4d52089baf6ebc62
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-02-13 20:49:12 +0800

    文本優化

--- cogs/game.py
+++ cogs/game.py
@@ -451,7 +451,7 @@ class BlackJack(commands.Cog):
         #玩家如果是blackjack(持有兩張牌且點數剛好為21)
         if self.calculate_point(player_cards) == 21:
             data[userid]['cake'] += int(bet + (bet*1.5))
-            message.add_field(name="結果",value=f"**BlackJack!**\n你獲得了**{int(bet*1.5)}**塊{cake_emoji}(blackjack! x 1.5)\n你現在有{data[userid]['cake']}塊{cake_emoji}",inline=False)
+            message.add_field(name="結果",value=f"**BlackJack!**\n你獲得了**{int(bet*1.5)}**塊{cake_emoji}(blackjack! x 1.5)\n你現在有**{data[userid]['cake']}**塊{cake_emoji}",inline=False)
             common.datawrite(data)
             await interaction.response.send_message(embed=message)
             return
