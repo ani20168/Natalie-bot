3e60437cfe33e7edf5b57983b4cb2458f0a5400a
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-04-04 14:58:34 +0800

    調整info的排版並添加新指令

--- cogs/general.py
+++ cogs/general.py
@@ -33,22 +33,18 @@ class General(commands.Cog):
                 common.datawrite(data)
 
             userlevel = common.LevelSystem().read_info(userid)
-            description = "你好!我是Natalie!\n你可以在這裡查看個人資料及指令表。"
-            message = Embed(title="我是Natalie!",description=description,color=common.bot_color)
+            message = Embed(title="我是Natalie!",description="你好!我是Natalie!\n你可以在這裡查看個人資料及指令表。",color=common.bot_color)
             message.add_field(name="個人資料",value=f"等級:**{userlevel.level}**  經驗值:**{userlevel.level_exp}**/**{userlevel.level_next_exp}**\n你有**{cake}**塊{self.bot.get_emoji(common.cake_emoji_id)}",inline=False)
+            
+            general_commands_list=f'/info 查看指令表及個人資料\n/eat 餵食Natalie\n/cake_give 給予他人{self.bot.get_emoji(common.cake_emoji_id)}\n/mining_info 挖礦小遊戲資訊\n/blackjack 21點遊戲\n/poll 簡單的投票功能'
+            #如果等級>=5 且沒有在 抽獎仔/VIP 身分內，則顯示指令
+            if userlevel.level >= 5 and all(role.id not in [621764669929160715, 605730134531637249] for role in interaction.user.roles):
+                general_commands_list += "\n/giveaway_join 加入抽獎頻道"
+            
+            message.add_field(name="指令表",value=general_commands_list,inline=False)
             message.add_field(
-                name="指令表",
-                value=f'''
-                /info 查看指令表及個人資料
-                /eat 餵食Natalie
-                /cake_give 給予他人{self.bot.get_emoji(common.cake_emoji_id)}
-                /mining_info 挖礦小遊戲資訊
-                /level_leaderboard 等級排行榜
-                /voice_leaderboard 語音活躍排行榜
-                /blackjack 21點遊戲
-                /blackjack_leaderboard 21點勝率排行榜
-                /poll 簡單的投票功能
-                ''',
+                name="排行榜",
+                value=f'/level_leaderboard 等級排行榜\n/voice_leaderboard 語音活躍排行榜\n/blackjack_leaderboard 21點勝率排行榜',
                 inline=False)
             await interaction.response.send_message(embed=message)
 
@@ -218,7 +214,7 @@ class General(commands.Cog):
             embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
             embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
             await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
-            
+
             async with common.jsonio_lock:
                 data = common.dataload()
                 #清除AFK狀態
