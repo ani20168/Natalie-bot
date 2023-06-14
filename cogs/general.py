a75a04c123eafc1cab98afca6efba9d1d32edab3
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-06-14 18:19:35 +0800

    由於API更新，修正顯示名稱

--- cogs/general.py
+++ cogs/general.py
@@ -214,14 +214,14 @@ class General(commands.Cog):
     #進入語音頻道
         if after.channel and not before.channel:
             embed = Embed(title="", description=f"{member.display_name} 進入了 {after.channel.name} 語音頻道", color=common.bot_color)
-            embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+            embed.set_author(name=f"{member.global_name}", icon_url=member.avatar)
             embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
             await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
 
         #離開語音頻道
         if before.channel and not after.channel:
             embed = Embed(title="", description=f"{member.display_name} 離開了 {before.channel.name} 語音頻道", color=common.bot_color)
-            embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+            embed.set_author(name=f"{member.global_name}", icon_url=member.avatar)
             embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
             await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
 
@@ -236,7 +236,7 @@ class General(commands.Cog):
         if before.channel != after.channel:
             if before.channel and after.channel:
                 embed = Embed(title="", description=f"{member.display_name} 從 {before.channel.name} 移動到 {after.channel.name} 頻道", color=common.bot_color)
-                embed.set_author(name=f"{member.name}#{member.discriminator}", icon_url=member.avatar)
+                embed.set_author(name=f"{member.global_name}", icon_url=member.avatar)
                 embed.timestamp = datetime.now(timezone(timedelta(hours=8)))
                 await self.bot.get_channel(common.mod_log_channel).send(embed=embed)
 
