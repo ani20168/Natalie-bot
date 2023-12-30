a9dd85f7986bd12b888d5dfe56758d64faa908d2
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-12-30 21:13:15 +0800

    check_sevencolor_restday debug

--- cogs/general.py
+++ cogs/general.py
@@ -37,7 +37,7 @@ class General(commands.Cog):
             message = Embed(title="我是Natalie!",description="你好!我是Natalie!\n你可以在這裡查看個人資料及指令表。",color=common.bot_color)
             message.add_field(name="個人資料",value=f"等級:**{userlevel.level}**  經驗值:**{userlevel.level_exp}**/**{userlevel.level_next_exp}**\n你有**{cake}**塊{self.bot.get_emoji(common.cake_emoji_id)}",inline=False)
             
-            general_commands_list=f'/info 查看指令表及個人資料\n/eat 餵食Natalie\n/cake_give 給予他人{self.bot.get_emoji(common.cake_emoji_id)}\n/mining_info 挖礦小遊戲資訊\n/blackjack 21點遊戲\n/poll 簡單的投票功能'
+            general_commands_list=f'/info 查看指令表及個人資料\n/eat 餵食Natalie\n/cake_give 給予他人{self.bot.get_emoji(common.cake_emoji_id)}\n/mining_info 挖礦小遊戲資訊\n/blackjack 21點遊戲\n/poll 簡單的投票功能\n/check_sevencolor_restday 確認七色有沒有休假'
             #如果等級>=5 且沒有在 抽獎仔/VIP 身分內，則顯示指令
             if userlevel.level >= 5 and all(role.id not in [621764669929160715, 605730134531637249] for role in interaction.user.roles):
                 general_commands_list += "\n/giveaway_join 加入抽獎頻道"
@@ -213,9 +213,9 @@ class General(commands.Cog):
     @app_commands.command(name = "check_sevencolor_restday", description = "確認七色珀的休假日")
     @app_commands.rename(date='日期')
     @app_commands.describe(date='輸入日期以查看當天是否休假，或著留空來查看他的下一次休假日期')
-    async def check_sevencolor_restday(self,interaction,checkarg:str,date:Optional[str] = None):
+    async def check_sevencolor_restday(self,interaction,date:Optional[str] = None):
         # 設定起始工作日
-        start_working_date = datetime(2023, 12, 26)
+        start_working_date = datetime(2023, 12, 28)
 
         # 工作和休息的週期（四天工作，兩天休息）
         work_days = 4
@@ -242,9 +242,9 @@ class General(commands.Cog):
 
                 # 判斷是否為休息日
                 if position_in_cycle >= work_days:
-                    await interaction.response.send_message(embed=Embed(title="查詢休假日...",description=f"七色在這天放假!({check_date})",color=common.bot_color))
+                    await interaction.response.send_message(embed=Embed(title="查詢休假日...",description=f"七色在這天放假! ({check_date.date()})",color=common.bot_color))
                 else:
-                    await interaction.response.send_message(embed=Embed(title="查詢休假日...",description=f"七色在這天沒有放假!({check_date})",color=common.bot_color))
+                    await interaction.response.send_message(embed=Embed(title="查詢休假日...",description=f"七色在這天沒有放假! ({check_date.date()})",color=common.bot_color))
             else:
                 # 查找下一個休息日的週期
                 days_since_start = (current_date - start_working_date).days
@@ -269,7 +269,7 @@ class General(commands.Cog):
                 rest_day_str_1 = relative_dates.get(date_diff_1, rest_day_1.strftime("%Y/%m/%d"))
                 rest_day_str_2 = relative_dates.get(date_diff_2, rest_day_2.strftime("%Y/%m/%d"))
 
-                await interaction.response.send_message(embed=Embed(title="最近的休假週期...",description=f"七色在{rest_day_str_1}跟{rest_day_str_2}放假!",color=common.bot_color))
+                await interaction.response.send_message(embed=Embed(title="最近的休假週期...",description=f"七色在 **{rest_day_str_1}** 跟 **{rest_day_str_2}** 放假!",color=common.bot_color))
         except ValueError:
             await interaction.response.send_message(embed=Embed(title="錯誤!",description="日期格式錯誤!",color=common.bot_error_color))
 
