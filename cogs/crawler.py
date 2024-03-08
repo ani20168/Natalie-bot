627b30ac90c918d2beb1f5a4fcbc9e09c4908a5a
ani20168 <50546458+ani20168@users.noreply.github.com>
2024-03-08 21:09:37 +0800

    Update crawler.py

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -31,7 +31,7 @@ class SteamFreeGameCrawler(commands.Cog):
         data = common.dataload()
         for needcheck_game in game_list:
             game_id = self.get_game_id(needcheck_game)
-            if game_id in data["steam_freegame_alreadypost"]:
+            if game_id in data.get("steam_freegame_alreadypost",[]):
                 game_list.remove(game_id)
         async with aiohttp.ClientSession() as session:
             tasks = [self.steam_check_free(session, game) for game in game_list]
