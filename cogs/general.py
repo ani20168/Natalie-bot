81a8f848ecb2626a8e4c7dd411109f8678e3b208
ani20168 <50546458+ani20168@users.noreply.github.com>
2025-04-27 16:14:25 +0800

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -162,13 +162,9 @@ class General(commands.Cog):
             await interaction.response.send_message(embed=Embed(title="加入抽獎頻道",description="你無法使用這個指令!\n你已經具備抽獎仔身分，或者等級不足以進入。",color=common.bot_error_color))
 
     @app_commands.command(description = "設置掛機斷連的觸發時間點(僅供部分會員使用)")
-    @app_commands.rename(timeset = "觸發時間")
-    @app_commands.describe(timeset = "何時觸發掛機斷連? 範圍為15~60分鐘")
+    # @app_commands.rename(timeset = "觸發時間")
+    # @app_commands.describe(timeset = "何時觸發掛機斷連? 範圍為15~60分鐘")
     async def afkdisconnect_trigger(self, interaction, timeset:int):
-        """
-        設置關MIC掛機後，被機器人中斷連線的觸發時間點
-
-        """
         userid = str(interaction.user.id)
         whitelist = [
             "410847926236086272" #ANI
