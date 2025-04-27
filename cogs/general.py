009dafe8d67901d1227219d9c0984a6956f6d3aa
ani20168 <50546458+ani20168@users.noreply.github.com>
2025-04-27 15:45:18 +0800

    update trigger command

--- cogs/general.py
+++ cogs/general.py
@@ -164,7 +164,7 @@ class General(commands.Cog):
     @app_commands.command(name = "afkdisconnect_trigger", description = "設置掛機斷連的觸發時間點(僅供部分會員使用)")
     @app_commands.rename(time = "觸發時間")
     @app_commands.describe(time = "何時觸發掛機斷連? range:15~60分鐘")
-    async def afk_disconnect_settrigger(self, interaction, time:int):
+    async def afkdisconnect_trigger(self, interaction, time:int):
         """
         設置關MIC掛機後，被機器人中斷連線的觸發時間點，
 
